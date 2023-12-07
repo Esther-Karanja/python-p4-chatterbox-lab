d4c1f3269c81b6b56c0a5c4a019922d6f7f2a2ce
Esther <est.karanja@gmail.com>
2023-12-07 14:27:42 +0300

    completed chatterbox lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,71 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods =['GET', 'POST'])
 def messages():
-    return ''
+    if request.method =='GET':
+        messages = []
 
-@app.route('/messages/<int:id>')
+        for message in Message.query.all():
+            messages.append(message.to_dict())
+
+        response = make_response(jsonify(messages),200)
+        response.headers["Content-Type"] = "application/json"
+        return response
+    
+    elif request.method =='POST':
+        message = request.get_json()
+        new_message = Message(
+        body= message['body'],
+        username =message['username']
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        response = make_response(
+                jsonify(new_message.to_dict()),
+                201
+            )
+        return response
+
+
+
+@app.route('/messages/<int:id>', methods =['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method =='PATCH':
+        update =request.get_json()
+        for attr in update:
+            setattr(message, attr, update[attr])
+
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(
+            jsonify(message_dict),
+            200
+        )
+        return response
+    
+    elif request.method == 'DELETE':
+        message = Message.query.filter_by(id=id).first()
+
+        db.session.delete(message)
+        db.session.commit()
+
+        response =make_response(
+            jsonify({
+                'message': 'message delated successfuly!'
+            })
+        ), 200
+        return response
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
---
+++ server/migrations/versions/b4c7291c291a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b4c7291c291a
+Revises: 
+Create Date: 2023-12-07 13:22:26.363195
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b4c7291c291a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
